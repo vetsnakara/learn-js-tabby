acfc88c5d8981dc43355d6f6674219c2660255f3
Konstantin Arakantsev <kostya3312@gmail.com>
2020-08-30 11:05:51 +0300

    use event delegation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const wrapper = document.querySelector('.tabby')
 
 const tabs = wrapper.querySelectorAll('.tab')
+const tabsContainer = wrapper.querySelector('.tabs')
 const tabContents = wrapper.querySelectorAll('.tab-content')
 
 const SELECTED_CLASS = 'is-selected'
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    // show selected tab
-    tabs.forEach(t => t.classList.remove(SELECTED_CLASS))
-    tab.classList.add(SELECTED_CLASS)
+tabsContainer.addEventListener('click', e => {
+  const tab = event.target
+  const target = tab.dataset.target
+  const tabContent = wrapper.querySelector(`#${target}`)
 
-    // show tab content
-    const tabContent = document.querySelector(`#${tab.dataset.target }`)
-    tabContents.forEach(c => c.classList.remove(SELECTED_CLASS))
-    tabContent.classList.add(SELECTED_CLASS)
-  })
+  // select tab
+  tabs.forEach(tab => tab.classList.remove(SELECTED_CLASS))
+  tab.classList.add(SELECTED_CLASS)
+
+  // select corresponding tab content
+  tabContents.forEach(content => content.classList.remove(SELECTED_CLASS))
+  tabContent.classList.add(SELECTED_CLASS)
 })
